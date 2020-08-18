57584c78e2ba1ee26cf778ba0f66352aba2d8474
in-in <dmin@tuta.io>
2020-08-18 14:17:49 +0300

    init commit

---
+++ src/PhotoGroove.elm
@@ -0,0 +1,44 @@
+module PhotoGroove exposing (main)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+urlPrefix =
+    "https://elm-in-action.com/"
+
+
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , hr [] []
+        , div [ id "thumbnails" ]
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ]
+
+
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        ]
+        []
+
+
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+main =
+    view initialModel
