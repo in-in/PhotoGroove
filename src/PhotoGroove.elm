591bca02031ffbf5559955a9fb682bd18a2c2f2d
in-in <dmin@tuta.io>
2020-11-21 15:30:39 +0300

    test(interactions): testing user interactions

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,4 +1,4 @@
-port module PhotoGroove exposing (Model, Msg(..), Photo, Status(..), initialModel, main, photoDecoder, photoFromUrl, update, urlPrefix, view)
+port module PhotoGroove exposing (Model, Msg(..), Photo, Status(..), initialModel, main, photoDecoder, update, urlPrefix, view)
 
 import Browser
 import Html exposing (..)
@@ -330,8 +330,3 @@ onSlide toMsg =
     at [ "detail", "userSlideTo" ] int
         |> Json.Decode.map toMsg
         |> on "slide"
-
-
-photoFromUrl : String -> Photo
-photoFromUrl url =
-    { url = url, size = 0, title = "" }
--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -1,19 +1,30 @@
-module PhotoGrooveTests exposing (..)
+module PhotoGrooveTests exposing
+    ( clickThumbnail
+    , decoderTest
+    , photoFromUrl
+    , sliders
+    , testSlider
+    , thumbnailRendered
+    , thumbnailsWork
+    , urlFuzzer
+    , urlsFromCount
+    )
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
-import Html.Attributes as Attr exposing (src)
-import Json.Decode as Decode exposing (decodeValue)
+import Fuzz exposing (Fuzzer, int, string)
+import Html.Attributes as Attr
+import Json.Decode exposing (decodeValue)
 import Json.Encode as Encode
-import PhotoGroove exposing (Model, Msg(..), Photo, Status(..), initialModel, photoFromUrl, update, urlPrefix, view)
-import Test exposing (Test, describe, fuzz, fuzz2)
-import Test.Html.Query as Query exposing (count)
-import Test.Html.Selector exposing (attribute, tag, text)
+import PhotoGroove exposing (Model, Msg(..), Photo, Status(..), initialModel, update, urlPrefix, view)
+import Test exposing (Test, describe, fuzz, fuzz2, fuzz3)
+import Test.Html.Event as Event
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (attribute, tag)
 
 
 decoderTest : Test
 decoderTest =
-    fuzz2 string int "title defaults to (untitled)" <|
+    fuzz2 string int "Title defaults to (untitled)" <|
         \url size ->
             [ ( "url", Encode.string url )
             , ( "size", Encode.int size )
@@ -44,18 +55,6 @@ testSlider description toMsg amountFromModel =
                 |> Expect.equal amount
 
 
-
--- noPhotosNoThumbnails : Test
--- noPhotosNoThumbnails =
---     test "No thumbnails render when there are no photos to render." <|
---         \_ ->
---             initialModel
---                 |> PhotoGroove.view
---                 |> Query.fromHtml
---                 |> Query.findAll [ tag "img" ]
---                 |> Query.count Expect.equal
-
-
 thumbnailRendered : String -> Query.Single msg -> Expectation
 thumbnailRendered url query =
     query
@@ -65,14 +64,9 @@ thumbnailRendered url query =
 
 thumbnailsWork : Test
 thumbnailsWork =
-    fuzz (Fuzz.intRange 1 5) "URLs render as thumbnails" <|
-        \urlCount ->
+    fuzz urlFuzzer "URLs render as thumbnails" <|
+        \urls ->
             let
-                urls : List String
-                urls =
-                    List.range 1 urlCount
-                        |> List.map (\num -> String.fromInt num ++ ".png")
-
                 thumbnailChecks : List (Query.Single msg -> Expectation)
                 thumbnailChecks =
                     List.map thumbnailRendered urls
@@ -83,3 +77,43 @@ thumbnailsWork =
                 |> view
                 |> Query.fromHtml
                 |> Expect.all thumbnailChecks
+
+
+photoFromUrl : String -> Photo
+photoFromUrl url =
+    { url = url, size = 0, title = "" }
+
+
+urlFuzzer : Fuzzer (List String)
+urlFuzzer =
+    Fuzz.intRange 1 5
+        |> Fuzz.map urlsFromCount
+
+
+urlsFromCount : Int -> List String
+urlsFromCount urlCount =
+    List.range 1 urlCount
+        |> List.map (\num -> String.fromInt num ++ ".png")
+
+
+clickThumbnail : Test
+clickThumbnail =
+    fuzz3 urlFuzzer string urlFuzzer "Clicking a thumbnail selects it" <|
+        \urlsBefore urlToSelect urlsAfter ->
+            let
+                url =
+                    urlToSelect ++ ".jpeg"
+
+                photos =
+                    (urlsBefore ++ url :: urlsAfter)
+                        |> List.map photoFromUrl
+
+                srcToClick =
+                    urlPrefix ++ url
+            in
+            { initialModel | status = Loaded photos "" }
+                |> view
+                |> Query.fromHtml
+                |> Query.find [ tag "img", attribute (Attr.src srcToClick) ]
+                |> Event.simulate Event.click
+                |> Event.expect (ClickedPhoto url)
